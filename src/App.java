b809f48f96135905a67342260b08c1959ac17991
dantalion-py <77492330j@gmail.com>
2023-09-19 11:59:10 +0800

    base

---
+++ src/App.java
@@ -0,0 +1,27 @@
+/**
+ * * requeriments:
+ * ? 1.-create account bank (checking account, savings account) 
+ * :
+ *      ? checking account:
+ *        * withdrawal
+ *        * deposit
+ * 
+ *      ? savings account:
+ *        * withdrawal
+ *        * deposit
+ *        * interest( 4% )
+ * 
+ *      ? 2000 account:
+ *        * withdrawal
+ *        * deposit
+ *        * interest( 8% )
+ *        ! Penalty for withdrawal of l 2% of the withdrawal
+ * 
+ *  */ 
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
